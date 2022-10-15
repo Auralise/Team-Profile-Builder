1b8e67ff0d861e97d2ac5e4a939f996419275d8a
James Prince <james.prince1@gmail.com>
2022-10-15 16:29:10 +1100

    Added basic Engineer class tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,33 @@
+const Engineer = include("../lib/Engineer");
+
+describe("Class ititialisation", () => {
+    it("Has all of the appropriate properties", () => {
+        const name = "Linus";
+        const id = 1;
+        const email = "linus@test.com";
+        const github = "torvalds"; 
+
+        const person = new Engineer(name, id, email, github);
+
+
+        expect(person.name).toBe("Linus");
+        expect(person.id).toBe(2);
+        expect(person.email).toBe("linus@test.com");
+        expect(person.github).toBe("torvalds");
+
+    });
+});
+
+describe("Class methods", () => {
+    it("returns the engineer's github username when getGithub() is called", () => {
+        const person = new Engineer("Simone", 3, "simone@test.com", "sim0n3");
+
+        expect(person.getGithub()).toBe("sim0n3");
+    });
+
+    it("returns the engineer's role when getRole() is called", () => {
+        const person = new Engineer("Pablo", 10, "pablo@test.com", "pabl0");
+
+        expect(person.getRole()).toBe("Engineer");
+    });
+});
