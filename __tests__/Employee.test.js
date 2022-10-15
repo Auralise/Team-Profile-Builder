e3ec6822b06fd2ea8e050d212a6724636d59fca5
James Prince <james.prince1@gmail.com>
2022-10-15 16:30:50 +1100

    Added proper line endings

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -14,31 +14,31 @@ describe("Initialisation", () => {
         expect(person.name).toBe("John");
         expect(person.id).toBe(2);
         expect(person.email).toBe("john@test.com");
-    })
-})
+    });
+});
 
 describe("Class methods", () => {
     it("returns employee name when getName() is called", () => {
         const person = new Employee("Sarah", 5, "sarah@test.com");
 
         expect(person.getName()).toBe("Sarah");
-    })
+    });
 
     it("returns Employee ID when getId() is called", () => {
         const person = new Employee("Steven", 9, "steven@test.com");
 
         expect(person.getId()).toBe(9);
-    })
+    });
 
     it("returns Employee email when getEmail() is called", () => {
         const person = new Employee("Fred", 8, "fred@test.com");
 
         expect(person.getEmail()).toBe("fred@test.com");
-    })
+    });
 
     it("returns Employee role when getRole() is called", () => {
         const person = new Employee("Ari", 4, "ari@test.com");
 
         expect(person.getRole()).toBe("Employee");
-    })
-})
+    });
+});
