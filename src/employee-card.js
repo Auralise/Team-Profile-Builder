9d074058ba3b91f2f8c6d76fb05ee60dae1aa1d4
James Prince <james.prince1@gmail.com>
2022-10-16 14:06:11 +1100

    Added HTML template styled with bootstrap to have array of cards replaced into. Created dynamic card builder

---
+++ src/employee-card.js
@@ -0,0 +1,50 @@
+const getClassSpecificAttr = employee => {
+    if (employee.hasOwnProperty("officeNumber")) {
+        return `
+            <li class="list-group-item">
+                Office Number: ${employee.getOfficeNum()}
+            </li>`;
+    } 
+    else if (employee.hasOwnProperty("github")) {
+        return `
+            <li class="list-group-item">
+                Github: <a href="https://github.com/${employee.getGithub()}">${employee.getGithub()}</a>
+            </li>`;
+    } 
+    else if (employee.hasOwnProperty("school")) {
+        return `
+            <li class="list-group-item">
+                School: ${employee.getSchool()}
+            </li>`;
+    } 
+    else {
+        return ``;
+    }
+}
+
+const buildEmployeeCard = employee => {
+
+    const card = `
+<section class="card m-3">
+    <div class="card-header bg-secondary text-light">
+        <h2>${employee.getName()}</h2>
+        <h3>&#x${employee.getLogo()}; ${employee.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Employee id: ${employee.getId()}
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a>
+            </li>
+            ${getClassSpecificAttr(employee)}
+        </ul>
+    </div>
+</section>`
+
+    return card;
+}
+
+
+exports.buildEmployeeCard = buildEmployeeCard;
